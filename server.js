a41754168239a29c035982e278c3d9037d3095bb
brookiecookie07 <45543480+brookiecookie07@users.noreply.github.com>
2018-12-02 15:31:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,48 @@ app.get('/', (req, res) => {
 });
 
 // your endpoint
+app.get('/pets', (req, res) => {
+  res.send("My puppy is very rolly-polly and named Teddy.");
+});
 
+app.get('/:name', (req, res) => {
+  var name = req.params.name;
+
+  res.send(`Hi, ${name}!`);
+});
 // scraper endpoint
+app.get('/hedgie/:keyword', (req, res) => {
+  var keyword = req.params.keyword;
+  function findHedgieImage(keyword) {
+    var nightmare = Nightmare();
+    return nightmare
+      .goto('https://www.google.com')
+      .insert('input[title="Search"]', `hedgehog ${keyword}`)
+      .click('input[value="Google Search"]')
+      .wait('a.q.qs')
+      .click('a.q.qs')
+      .wait('div#res.med')
+      .evaluate(function() {
+        var photoDivs = document.querySelectorAll('img.rg_ic');
+        var list = [].slice.call(photoDivs);
 
+        return list.map(function(div) {
+          return div.src;
+        });
+        })
+        .end()
+        .then(function (result) {
+          return result.slice(1, 5);
+        })
+        .then(function (images) {
+          res.json(images);
+        })
+        .catch(function (error) {
+          console.error('Search failed:', error);
+        });
+  }
+  findHedgieImage(keyword);
+});
 app.listen(port, () => {
   console.log(`app running on ${port}`);
 });
